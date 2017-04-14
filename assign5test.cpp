66f2fcd254894f38fd5fc3645245168a6b08222b
iboatwright <ijboatwright@gmail.com>
2017-04-14 11:37:44 -0400

    fixing epsilon validation/error messages

--- assign5test.cpp
+++ assign5test.cpp
@@ -50,8 +50,11 @@ int main(int argc, char* argv[]) {
   // number of equations to calculate
   double epsilon = validateEpsilon(argc, argv[1]);
   if (!epsilon) {
-    std::cout << "\t\t Execution: " << prog_name << " " << 
-              ((argc < 2)? "no epsilon": epsilon) << std::endl;
+    std::cout << "\t\t Execution: " << prog_name << " ";
+    
+    if (argc < 2) std::cout << "no epsilon" << std::endl;
+    else std::cout << argv[1] << std::endl;
+    
     return 1;
   }
   
